ae7119bcf2e53786c8d656604195f268a66d7dda
Danilo Augusto Torres <torres.danilo5@gmail.com>
2020-11-16 14:00:17 -0300

    inicio projeto
    inicio projeto

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulário de Inscrição</title>
+</head>
+<body>
+    <p>Formulário de Inscrição de Competidores</p>
+    <form action="script.php" method="post">
+        <p>Seu Nome: <input type="text" name="nome" ></p>
+        <p>Sua Idade: <input type="text" name="idade" ></p>
+        <p><input type="submit" value="Enviar Dados do Competidor"></p>
+    </form>
+</body>
+</html>
---
+++ script.php
@@ -0,0 +1,35 @@
+<?php
+    $categorias = [];
+    $categorias[] = 'Infantil';
+    $categorias[] = 'Adolescentes';
+    $categorias[] = 'Adulto';
+    $categorias[] = 'Idoso';
+    $nome = $_POST['nome'];
+    $idade = $_POST['idade'];
+
+  
+    if($idade >= 6  && $idade <= 12)
+    {
+        for($i = 0; $i <= count($categorias); $i++)
+        {
+            if($categorias[$i] == 'Infantil')
+                echo "O nadador $nome compete na categoria ".$categorias[$i];
+        }
+    }
+    else if($idade >= 13 && $idade <= 18)
+    {
+        for($i = 0; $i <= count($categorias); $i++)
+        {
+            if($categorias[$i] == 'Adolescentes')
+                echo "O nadador ".$nome." compete na categoria".$categorias[$i];
+        }
+    }else{
+        for($i = 0; $i <= count($categorias); $i++)
+        {
+            if($categorias[$i] == 'Adulto')
+            {
+                echo "O nadador $nome compete na categoria " .$categorias[$i];
+            }
+        }
+    }
+    
